10abf644edb760d231d1b8fd50f6c503d315e6e7
Matthew Phan <mttphan261@gmail.com>
2023-08-15 13:45:13 -0500

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,50 +1,117 @@
 import { useState, React } from "react";
-import { CarouselItem, Image } from "react-bootstrap";
-import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Projects() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
-
   return (
-    <Carousel activeIndex={index} on Select={handleSelect}>
-      <Carousel.Item>
-        <Image
-          src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/labassistantfolio.png"
-          alt="Lab Assistant"
-          className="project-image"
-        />
-        <Carousel.Caption>
-          <h3>The Street Fighter Lab Assistant</h3>
-          <p>Training companion for Street Fighter 6. View and personalize resources such as notes, matchups and video libraries.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/labassistantfolio.png"
-          alt="Lab Assistant"
-          className="project-image"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/labassistantfolio.png"
-          alt="Lab Assistant"
-          className="project-image"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <>
+      <h2 className="section-header">Projects</h2>
+      <div className="project-wrapper">
+        <Row sm={1} md={3}>
+          <Col>
+            <Card className="project-card">
+              <Card.Img src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/Artboard%201.png" />
+              <Card.Body className="project-body">
+                <Card.Title>The Street Fighter Lab Assistant</Card.Title>
+                <Card.Text>
+                  Resource for Street Fighter players, allowing them to track
+                  and enhance their training by accessing character information,
+                  movelists, user-submitted videos, and personalized notes.
+                </Card.Text>
+                <div className='project-skills'>
+                <span className="project-skill">
+                    React
+                </span>
+                <span className="project-skill">
+                    React-Bootstrap
+                </span>
+                <span className="project-skill">
+                    Flask
+                </span>
+                <span className="project-skill">
+                    Flask-Restful
+                </span>
+                <span className="project-skill">
+                    SQLAlchemy
+                </span>
+                <span className="project-skill">
+                    PostgreSQL
+                </span>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="project-card">
+              <Card.Img src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/gunplapal.png" />
+              <Card.Body>
+                <Card.Title>Gunpla Pal</Card.Title>
+                <Card.Text>
+                  Dedicated database and collection tracker for Gunpla
+                  enthusiasts. A comprehensive collection of Gunpla kits for
+                  users to explore, track and manage in their collection.
+                </Card.Text>
+                <div className='project-skills'>
+                <span className="project-skill">
+                    React
+                </span>
+                <span className="project-skill">
+                    React-Bootstrap
+                </span>
+                <span className="project-skill">
+                    Flask
+                </span>
+                <span className="project-skill">
+                    Flask-Restful
+                </span>
+                <span className="project-skill">
+                    SQLAlchemy
+                </span>
+                <span className="project-skill">
+                    PostgreSQL
+                </span>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="project-card">
+              <Card.Img src="https://raw.githubusercontent.com/Mttphan261/portfolio/main/.github/imgs/bitesnbrews.png" />
+              <Card.Body>
+                <Card.Title>Bites & Brews</Card.Title>
+                <Card.Text>
+                  Recipe/Cocktail site that allows users to browse meal and
+                  cocktail recipes, complete with their ingredients list and
+                  directions. Allows for favoriting and submitting of recipes as
+                  well as a pairing recommendation.
+                </Card.Text>
+                <div className='project-skills'>
+                <span className="project-skill">
+                    React
+                </span>
+                <span className="project-skill">
+                    React-Bootstrap
+                </span>
+                <span className="project-skill">
+                    Flask
+                </span>
+                <span className="project-skill">
+                    Flask-Restful
+                </span>
+                <span className="project-skill">
+                    SQLAlchemy
+                </span>
+                <span className="project-skill">
+                    PostgreSQL
+                </span>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 }
 
